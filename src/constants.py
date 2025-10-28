987affa18eb019631e9127f8aff4522aa6a333b5
rbmr <robertmertens10@gmail.com>
2025-10-28 16:06:27 +0100

    feat: add tqdm logging handler

--- src/constants.py
+++ src/constants.py
@@ -1,9 +1,29 @@
+import logging
 from pathlib import Path
+
+from tqdm import tqdm
 
 SRC_DIR = Path(__file__).resolve().parent
 PROJECT_DIR = SRC_DIR.parent
 ENV_PATH = PROJECT_DIR / ".env"
 STATE_FILE = PROJECT_DIR / "state.json"
+
+class TqdmLoggingHandler(logging.StreamHandler):
+    """
+
+    """
+    def __init__(self):
+        super().__init__()
+
+    def emit(self, record):
+        try:
+            msg = self.format(record)
+            tqdm.write(msg, file=self.stream)
+            self.flush()
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
 
 LOGGING_CONFIG = {
     'version': 1,
@@ -14,14 +34,19 @@ LOGGING_CONFIG = {
         }
     },
     'handlers': {
+        'tqdm': {
+            'class': TqdmLoggingHandler,
+            'formatter': 'default',
+            'level': logging.INFO,
+        },
         'console': {
             'class': 'logging.StreamHandler',
             'formatter': 'default',
-            'level': 'INFO',
+            'level': logging.INFO,
         }
     },
     'root': {
-        'handlers': ['console'],
-        'level': 'INFO',
+        'level': logging.INFO,
+        'handlers': ['tqdm'],
     }
 }
