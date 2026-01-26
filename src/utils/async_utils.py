01c2349b28ddc61c3f1d309273cce51ebe5c8b3e
rbmr <robertmertens10@gmail.com>
2026-01-26 20:46:37 +0100

    refactor: use dataclasses for progress bars in async_utils.py

--- src/utils/async_utils.py
+++ src/utils/async_utils.py
@@ -2,22 +2,29 @@
 
 import asyncio
 import logging
-from typing import List, TypeVar, Coroutine, Any, Optional
+from dataclasses import asdict, dataclass
+from typing import Any, Coroutine, List, Optional, TypeVar
 
-from pydantic import BaseModel
 from tqdm.asyncio import tqdm as async_tqdm
 
 logger = logging.getLogger(__name__)
 
-T = TypeVar('T')
+T = TypeVar("T")
 
-class PBarConfig(BaseModel):
+
+@dataclass(frozen=True)
+class PBarConfig:
     """A typed configuration for tqdm progress bars."""
+
     desc: str
     unit: str
     leave: bool = False
+    colour: Optional[str] = None
 
-def limit_conc(tasks: List[Coroutine[Any, Any, T]], limit: int) -> List[Coroutine[Any, Any, T]]:
+
+def limit_conc(
+    tasks: List[Coroutine[Any, Any, T]], limit: int
+) -> List[Coroutine[Any, Any, T]]:
     """Wraps a list of tasks using the same semaphore with a specified limit."""
     semaphore = asyncio.Semaphore(limit)
 
@@ -28,29 +35,32 @@ def limit_conc(tasks: List[Coroutine[Any, Any, T]], limit: int) -> List[Coroutin
 
     return [worker(task) for task in tasks]
 
+
 async def run_async_tasks(
-        tasks: List[Coroutine[Any, Any, T]],
-        limit: int = 0,
-        pbar: Optional[PBarConfig] = None,
+    tasks: List[Coroutine[Any, Any, T]],
+    limit: int = 0,
+    pbar: Optional[PBarConfig] = None,
 ) -> List[T]:
     """
     Runs a list of awaitable tasks concurrently with a specified concurrency limit.
     """
     if limit < 0:
-        raise ValueError('limit must non-negative')
+        raise ValueError("limit must non-negative")
 
     if not tasks:
         return []
 
     if limit > 0:
         tasks = limit_conc(tasks, limit)
-        logger.info(f"Running {len(tasks)} tasks with a concurrency limit of {limit}...")
+        logger.info(
+            f"Running {len(tasks)} tasks with a concurrency limit of {limit}..."
+        )
     else:
         logger.info(f"Running {len(tasks)} tasks concurrently...")
 
     if pbar is None:
         return await asyncio.gather(*tasks)
 
-    kwargs = pbar.model_dump()
+    kwargs = asdict(pbar)
     kwargs["total"] = len(tasks)
     return await async_tqdm.gather(*tasks, **kwargs)
